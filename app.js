c653fad2e4010e8549556c9a9e2ad9426190db6c
Supreet <me.supi@gmail.com>
2020-02-06 23:56:04 -0500

    Created Server

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+app.use(morgan("common"));
+
+const playstore = require("./playstore-data.js");
+
+app.get("/apps", (req, res) => {
+    const { sort, genres = "" } = req.query;
+    if (sort) {
+        if (!["rating", "app"].includes(sort)) {
+            return res.status(400).send("Sort must be one of rating or app");
+        }
+    }
+    if (genres) {
+        if (!["Action", "Puzzle", "Strategy", "Cascade", "Arcade", "Card"].includes(
+                genres
+            )) {
+            return res
+                .status(400)
+                .send(
+                    "Genres must be one of Action, Puzzle, Strategy, Cascade, Arcade or Card"
+                );
+        }
+    }
+    let results = playstore.filter(playstore =>
+        playstore.Genres.toLowerCase().includes(genres.toLowerCase())
+    );
+    if (sort) {
+        results.sort((a, b) => {
+            return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
+        });
+    }
+    res.json(results);
+});
+
+app.listen(8000, () => {
+    console.log("Server started on PORT 8000");
+});
